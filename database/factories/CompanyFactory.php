00bea7b516c27be9655fef8c3e91d754f439594e
ZeroToH3ro <mtblaser2002@gmail.com>
2023-12-14 18:25:24 +0700

    job finder final project

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'cname' => $name = $this->faker->company(),
+            'slug' => Str::slug($name),
+            'address' => $this->faker->address(),
+            'phone' => $this->faker->phoneNumber(),
+            'logo' => 'gridtemplate.png',
+            'banner' => 'banner.png',
+            'slogan' => 'Work Hard Paid Off',
+            'website' => 'https://github.com/ZeroToH3ro/ZeroToH3ro',
+            'description' => $this->faker->paragraph(rand(2, 10))
+        ];
+    }
+}
